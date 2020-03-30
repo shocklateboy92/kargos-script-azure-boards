574a1b9d72a828c8032b641f3200737bb5b44a0b
Lasath Fernando <devel@lasath.org>
2020-03-29 21:17:31 -0700

    Add missing files from prev commit

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,26 +1,26 @@
-import webpack from "webpack";
-import path from "path";
+import webpack from 'webpack';
+import path from 'path';
 
 const config: webpack.Configuration = {
-    entry: "./src/main.ts",
+    entry: './src/main.ts',
     output: {
         path: path.resolve(
-            "./pkg/kargos-script-azure-boards/usr/local/lib/js/"
+            './pkg/kargos-script-azure-boards/usr/local/lib/js/'
         ),
-        filename: "kargos-script-azure-boards.js"
+        filename: 'kargos-script-azure-boards.js'
     },
-    mode: "development",
-    target: "node",
+    mode: 'development',
+    target: 'node',
     module: {
         rules: [
             {
                 test: /\.ts$/,
-                use: "ts-loader"
+                use: 'ts-loader'
             }
         ]
     },
     resolve: {
-        extensions: [".ts", ".js"]
+        extensions: ['.ts', '.js']
     },
     stats: {
         // Ignore warnings due to yarg's dynamic module loading
